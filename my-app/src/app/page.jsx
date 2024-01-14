a0a71436e77783912508076d7d34a0ae5f477907
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-01-14 23:02:59 +0600

    added home page heading

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center ">Discover & Share</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI-powered Prompts</span>
+    </section>
+  );
 };
 export default Home;
