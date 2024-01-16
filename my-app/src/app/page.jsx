749f1e8903f8f486dcaf61fdeca7f736d58086fe
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-01-16 19:39:54 +0600

    added heading

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <section className="w-full flex-center flex-col">
-      <h1 className="head_text text-center ">Discover & Share</h1>
-      <br className="max-md:hidden" />
-      <span className="orange_gradient text-center">AI-powered Prompts</span>
+    <section className="w-full text-center">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is a open-source AI prompting tool for modern world to
+        discover, create and share creative prompts
+      </p>
     </section>
   );
 };
 export default Home;
+// watched 56min
