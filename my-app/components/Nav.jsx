5ddf08c5c8205401275f949207cc742d92ecdb97
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-01-17 19:07:03 +0600

    added create post button

--- my-app/components/Nav.jsx
+++ my-app/components/Nav.jsx
@@ -1,8 +1,35 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 export const Nav = () => {
-  return <div>Nav</div>;
+  const userLoggedIn = true;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="Promptopia Logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        ></Image>
+        <p className="logo_text">Promptopia</p>
+        {/* Desktop navigation */}
+        <div className="sm:flex hidden">
+          {userLoggedIn ? (
+            <div className="flex gap-3 md:gap-5">
+              <Link href="/create-prompt" className="black_btn">
+                Create Post
+              </Link>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </Link>
+    </nav>
+  );
 };
