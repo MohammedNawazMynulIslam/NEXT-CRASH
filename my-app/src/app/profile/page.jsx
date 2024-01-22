fceae38e7f30a67d2ca6c5292f37d638e1d62f7e
MohammedNawazMynulIslam <mynulislam183@gmail.com>
2024-01-22 16:05:32 +0600

    added edit post page

--- my-app/src/app/profile/page.jsx
+++ my-app/src/app/profile/page.jsx
@@ -6,6 +6,7 @@ import { Profile } from "@components/Profile";
 const MyProfile = () => {
   const { data: session } = useSession();
   const [posts, setPosts] = useState([]);
+  const router = useRouter();
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await fetch(`/api/users/${session?.user?.id}/posts`);
@@ -14,8 +15,10 @@ const MyProfile = () => {
     };
     if (session?.user?.id) fetchPosts();
   }, []);
-  const handleEdit = () => {};
-  const handleDelete = async () => {};
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`);
+  };
+  const handleDelete = async (post) => {};
   return (
     <Profile
       name="My"
